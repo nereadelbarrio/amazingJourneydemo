14b7d6de9d62262ab107cc5d3313989af4f20139
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2022-06-14 17:48:41 +0200

    Update style.js

--- style.js
+++ style.js
@@ -38,6 +38,7 @@
 				{ name: 'Énfasis 06', element: 'span', attributes: { 'class': 'bck-enfasis-6'} },
 				{ name: 'Énfasis 07', element: 'span', attributes: { 'class': 'bck-enfasis-7'} },
 				{ name: 'Énfasis 08', element: 'span', attributes: { 'class': 'bck-enfasis-8'} },
+				{ name: 'Énfasis 09', element: 'span', attributes: { 'class': 'bck-enfasis-9'} },
 				
 		
 				{ name: 'Lista Desordenada 01', element: 'ul', attributes: { 'class': 'bck-ul bck-ul-1' } },
