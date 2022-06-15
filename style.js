2af3decc1726d81dde2be37002af17712282e5d8
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2022-06-15 09:54:29 +0200

    Update style.js

--- style.js
+++ style.js
@@ -55,6 +55,7 @@
 				{ name: 'Caja 01', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-1' } },
 				{ name: 'Caja 02', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-2' } },
 				{ name: 'Caja 03', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-3' } },
+				{ name: 'Caja 04', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-4' } },
 				
 				
 				{ name: 'Icono Hablar01', element: 'span', attributes: { 'class': 'icon icon-hablar01' } },
